5a09e71fe1dc30874f034ee7c6d2ba46195e2ed4
Vadim-A <golovachyov@gmail.com>
2016-06-16 00:47:27 +0500

    1st
    1st commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+var app = angular.module('app', []);
+
+//lesson 2
+app.controller('myCtrl', function ($scope) {
+	$scope.hello = 55;
+	$scope.myBook = 'AngularJS';
+});
+
+//lesson 3
+app.factory('myFactory', function () {
+    return {
+        hello: 'hello world'
+    }
+});
+
+app.controller('firstCtrl', function($scope, myFactory) {
+  console.log('firstCtrl');
+  $scope.myFactory = myFactory;
+  $scope.hello = 'hello world';
+});
+
+app.controller('secondCtrl', function($scope, myFactory) {
+  console.log('secondCtrl');
+  $scope.myFactory = myFactory;
+  $scope.hello = 'hello world';
+});
+
+//lesson 4
+app.controller('ctrl_l4', function($scope, myFactory_l4) {
+	$scope.hello = 'hello world';
+	$scope.myFactory = myFactory_l4;
+	$scope.getBookmark = function () {
+		return 'My bookmark';
+	};
+	$scope.setHello = function (text) {
+		$scope.hello = text;
+	};
+});
+
+app.factory('myFactory_l4', function () {
+  return {
+    hello: function () {
+      return 'hello world111';
+    }
+  }
+})
